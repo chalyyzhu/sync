c82bddd41b646bb99322816d94052b098013de9e
chalyyzhu <39033248+chalyyzhu@users.noreply.github.com>
2022-10-03 18:10:30 +0700

    Ok

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+DIR="/root/sync"
+
+if [ -d "$DIR" ]; then
+   rm -rf "$DIR"
+   mkdir -p "$DIR"
+   echo "Renstalling .. [+]"
+else
+   mkdir -p "$DIR"
+   echo "Installing .. [+]"  
+fi
+
+opkg update
+
+wget "https://raw.githubusercontent.com/chalyyzhu/sync/main/bug.txt" -O "${DIR}/bug.txt"
+wget "https://raw.githubusercontent.com/chalyyzhu/sync/main/config.json" -O "${DIR}/config.json"
+
+wget "https://raw.githubusercontent.com/chalyyzhu/sync/main/autostart-sync" -O /etc/init.d/autostart-sync
+wget "https://raw.githubusercontent.com/chalyyzhu/sync/main/sync" -O /usr/bin/sync
+
+chmod +x /usr/bin/sync
+chmod +x /etc/init.d/autostart-sync 
+/etc/init.d/autostart-sync enable
+/etc/init.d/autostart-sync start
+
+wget "https://raw.githubusercontent.com/chalyyzhu/sync/main/opreker" -O /usr/bin/opreker
+chmod +x /usr/bin/opreker
+
+echo "INSTALATION SUCESS.. [+]"
+rm install.sh
